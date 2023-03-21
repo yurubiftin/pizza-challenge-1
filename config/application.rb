b06ef77b5556007c9e27064fb3e9a97fccea35b8
yurubahmed <yurubiftin2002@gmail.com>
2023-03-21 10:05:24 -0400

    disale sqlite3 warning

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module PizzaShop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.active_record.sqlite3_production_warning = false
 
     # Configuration for the application, engines, and railties goes here.
     #
