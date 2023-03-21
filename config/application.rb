de247c70db93d919842b558cb51b456d847a32cd
yurubahmed <yurubiftin2002@gmail.com>
2023-03-21 14:26:34 -0400

    commented lines of code in app.rb

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,8 @@ Bundler.require(*Rails.groups)
 module PizzaShop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-    config.active_record.sqlite3_production_warning = false
+    # config.load_defaults 6.1
+    # config.active_record.sqlite3_production_warning = false
 
     # Configuration for the application, engines, and railties goes here.
     #
